1460a2260aca9e852b8acd15f2d90bbd033fe411
yang1014-st <yang1014@stthomas.edu>
2020-12-25 22:58:43 -0600

    DTT-TestPredictableDie-FirstRed

---
+++ SkunkProject/test/skunk/domain/TestPredictableDie.java
@@ -0,0 +1,16 @@
+package skunk.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPredictableDie
+{
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
