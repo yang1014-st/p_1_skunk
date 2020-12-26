496f74b96f2d0322e1cb72b9987bcac227d4efde
yang1014-st <yang1014@stthomas.edu>
2020-12-26 00:00:56 -0600

    test_PD_1_2_3_1 - Green

--- SkunkProject/test/skunk/domain/TestPredictableDie.java
+++ SkunkProject/test/skunk/domain/TestPredictableDie.java
@@ -23,16 +23,15 @@ class TestPredictableDie
 	void test_PD_1_2_3_1()
 	{
 		
-		fail();
-//		PredictableDie die = new PredictableDie();
-//		die.roll();
-//		assertEquals(1,die.getLastRoll());
-//		die.roll();
-//		assertEquals(2,die.getLastRoll());
-//		die.roll();
-//		assertEquals(3,die.getLastRoll());
-//		die.roll();
-//		assertEquals(1,die.getLastRoll());
+		PredictableDie die = new PredictableDie();
+		die.roll();
+		assertEquals(1,die.getLastRoll());
+		die.roll();
+		assertEquals(2,die.getLastRoll());
+		die.roll();
+		assertEquals(3,die.getLastRoll());
+		die.roll();
+		assertEquals(1,die.getLastRoll());
 	}
 
 
