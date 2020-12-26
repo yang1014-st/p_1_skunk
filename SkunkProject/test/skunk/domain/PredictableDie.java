9fe4d145c0f3e320c7320a53ff3d20467e4744f1
yang1014-st <yang1014@stthomas.edu>
2020-12-25 23:53:03 -0600

    test_PD_1_2_3 - Red

--- SkunkProject/test/skunk/domain/PredictableDie.java
+++ SkunkProject/test/skunk/domain/PredictableDie.java
@@ -3,21 +3,11 @@ package skunk.domain;
 public class PredictableDie extends Die
 {
 
-	private int[] theRolls;
-	private int nextInt;
 
-
-	
-	public PredictableDie(int[] is)
-	{
-		this.theRolls = is;
-		this.roll();
-	}
 
 	public void roll()
 	{
-		this.nextInt=0;
-		this.lastRoll = 1;
+		
 	}
 
 
--- SkunkProject/test/skunk/domain/TestPredictableDie.java
+++ SkunkProject/test/skunk/domain/TestPredictableDie.java
@@ -10,11 +10,11 @@ class TestPredictableDie
 	@Test
 	void test_PD_1_2_3()
 	{
-		PredictableDie die = new PredictableDie( new int [] {1,2,3});
+		PredictableDie die = new PredictableDie();
 		die.roll();
 		assertEquals(1,die.getLastRoll());
-		die.roll();
-		assertEquals(2,die.getLastRoll());
+//		die.roll();
+//		assertEquals(2,die.getLastRoll());
 	}
 
 }
