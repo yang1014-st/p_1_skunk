7c8a0dae7d61da07f4d0fff9da3bdafc6d744614
yang1014-st <yang1014@stthomas.edu>
2020-12-26 00:15:52 -0600

    TestPredictableDice - test_PD_1_2_3_1 - Red

--- SkunkProject/test/skunk/domain/TestPredictableDice.java
+++ SkunkProject/test/skunk/domain/TestPredictableDice.java
@@ -16,9 +16,21 @@ class TestPredictableDice
 		assertEquals(4,dice.getLastRoll());
 		dice.roll();
 		assertEquals(6,dice.getLastRoll());
-
  
-		
 	}
+	
+	@Test
+	void test_PD_1_2_3_1()
+	{
+		fail();
+//		PredictableDice dice = new PredictableDice ();
+//		assertEquals(2,dice.getLastRoll());
+//		dice.roll();
+//		assertEquals(4,dice.getLastRoll());
+//		dice.roll();
+//		assertEquals(6,dice.getLastRoll());
+ 
+	}
+
 
 }
