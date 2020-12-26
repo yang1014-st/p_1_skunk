59050549387113ad8a60ba3088df7c98899bb0e6
yang1014-st <yang1014@stthomas.edu>
2020-12-25 23:57:15 -0600

    test_PD_1_2_3 - First Roll Green

--- SkunkProject/test/skunk/domain/PredictableDie.java
+++ SkunkProject/test/skunk/domain/PredictableDie.java
@@ -3,10 +3,22 @@ package skunk.domain;
 public class PredictableDie extends Die
 {
 
-
+	private int testRoll = 0;
 
 	public void roll()
 	{
+		
+		if (testRoll ==3) {
+			this.setLastRoll(1);
+		}
+		else {
+			this.setLastRoll(testRoll+1);
+		}
+	}
+
+	private void setLastRoll(int i)
+	{
+		super.lastRoll = i;
 		
 	}
 
