847105b290411fe8c7314753a0d0fa5c4309152f
yang1014-st <yang1014@stthomas.edu>
2020-12-25 21:37:26 -0600

    Create, stage, and commit a main() Java class SkunkApp

---
+++ SkunkProject/src/skunk/domain/SkunkApp.java
@@ -0,0 +1,14 @@
+package skunk.domain;
+import edu.princeton.cs.introcs.*;
+
+public class SkunkApp {
+
+	public static void main(String[] args) {
+		StdOut.println("Welcome to play Skunk!");
+		Dice dice = new Dice();
+		dice.roll();
+		StdOut.println(dice.toString());
+
+	}
+
+}
