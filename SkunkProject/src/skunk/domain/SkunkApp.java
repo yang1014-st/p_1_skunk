4c827191c1265bb3c3665793ad6ccd2c7fa16b0c
yang1014-st <yang1014@stthomas.edu>
2020-12-25 22:06:12 -0600

    Use Preferred format

--- SkunkProject/src/skunk/domain/SkunkApp.java
+++ SkunkProject/src/skunk/domain/SkunkApp.java
@@ -1,9 +1,12 @@
 package skunk.domain;
+
 import edu.princeton.cs.introcs.*;
 
-public class SkunkApp {
+public class SkunkApp
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		StdOut.println("Welcome to play Skunk!");
 		Dice dice = new Dice();
 		dice.roll();
