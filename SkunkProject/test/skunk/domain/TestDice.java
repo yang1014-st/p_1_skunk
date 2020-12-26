7799705d8e207522c2de92708a663236e1ce5578
yang1014-st <yang1014@stthomas.edu>
2020-12-26 01:56:55 -0600

    TestDice - test_predictable_dice - Red

---
+++ SkunkProject/test/skunk/domain/TestDice.java
@@ -0,0 +1,14 @@
+package skunk.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestDice {
+
+	@Test
+	void test_predictable_dice() {
+		fail("Not yet implemented");
+	}
+
+}
