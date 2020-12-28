401c782d2a0851475e8b5b93bdcfebac8a7bbec4
yang1014-st <yang1014@stthomas.edu>
2020-12-27 22:01:37 -0600

    TestRoll - test_regular_skunk - Red

---
+++ SkunkProject/test/skunk/domain/TestRoll.java
@@ -0,0 +1,14 @@
+package skunk.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestRoll {
+
+	@Test
+	void test_regular_skunk() {
+		fail("Not yet implemented");
+	}
+
+}
