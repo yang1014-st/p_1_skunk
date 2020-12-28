03828f8720984b063ec48e5714ce419f5688c2bd
yang1014-st <yang1014@stthomas.edu>
2020-12-27 21:51:16 -0600

    TestDice - test_predictable_dice_1and2 - Red

--- SkunkProject/test/skunk/domain/TestDice.java
+++ SkunkProject/test/skunk/domain/TestDice.java
@@ -10,11 +10,26 @@ class TestDice {
 	void test_predictable_dice_1and1() { 
 
 		PredictableDie die1 = new PredictableDie();
+		die1.roll();
 		Dice dice = new Dice (die1, die1);
 		dice.roll();
 		assertEquals(2, dice.getLastRoll());
 		assertEquals("Dice with last roll: 2 => 1 + 1",dice.toString());
 	
 	}
+	
+	@Test
+	void test_predictable_dice_1and2() { 
+
+		PredictableDie die1 = new PredictableDie();
+		die1.roll();
+		Dice dice = new Dice (die1, die1);
+		dice.roll();
+		assertEquals(3, dice.getLastRoll());
+		assertEquals("Dice with last roll: 3 => 2 + 1",dice.toString());
+	
+	}
+	
+	
 
 }
