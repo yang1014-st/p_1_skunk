db342560def7c186c6d99725fc119eb6d8afbbfe
yang1014-st <yang1014@stthomas.edu>
2020-12-31 12:41:51 -0600

    TestTurn - test_with_predictable_die_1and3 - Red

--- SkunkProject/test/skunk/domain/TestTurn.java
+++ SkunkProject/test/skunk/domain/TestTurn.java
@@ -29,5 +29,20 @@ class TestTurn {
 		Turn turn = new Turn(true,roll);
 		assertEquals(0,turn.get_turn_score());
 	}
+	
+	@Test
+	void test_with_predictable_die_1and3() {
+		fail();
+		PredictableDie die1 = new PredictableDie();
+		PredictableDie die2 = new PredictableDie();
+		die1.roll();
+		die2.roll();
+		die2.roll();
+		die2.roll();
+		Dice dice = new Dice(die1, die2);
+		Roll roll = new Roll(dice);
+		Turn turn = new Turn(true,roll);
+		assertEquals(0,turn.get_turn_score());
+	}
 
 }
