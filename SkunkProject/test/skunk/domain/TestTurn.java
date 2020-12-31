14a1caf227522d48de42c4a70156bb7c3b4af48c
yang1014-st <yang1014@stthomas.edu>
2020-12-31 14:27:08 -0600

    TesTurn - test_want_to_stop - Red

--- SkunkProject/test/skunk/domain/TestTurn.java
+++ SkunkProject/test/skunk/domain/TestTurn.java
@@ -166,4 +166,11 @@ class TestTurn {
 		assertFalse(turn.get_turn_score() ==turn_score_1);
 	}
 
+	@Test
+	void test_want_to_stop() {
+		Turn turn = new Turn();
+		int turn_score_1 = turn.get_turn_score();
+		turn.want_to_stop();
+		assertTrue(turn.get_turn_score() !=turn_score_1);
+	}
 }
