46d201c48b31080b60ba7b9950ed468ce164c983
yang1014-st <yang1014@stthomas.edu>
2021-01-01 00:15:54 -0600

    test_1turn_enter_y - Red

--- SkunkProject/test/myskunk/dl/TestController.java
+++ SkunkProject/test/myskunk/dl/TestController.java
@@ -27,6 +27,16 @@ class TestController {
 				+"Your scope is 0. You have 50 chips.", controller.get_result());
 	}
 	
+	@Test
+	void test_1turn_enter_y() {
+		Controller controller = new Controller();
+		controller.ask_user_if_want_to_roll(true, 'y');
+		controller.run();
+		assertEquals("You did not select 'y'." + "\n" 
+				+"End of the turn. Score for this turn is 0. Chip penality is 0." + "\n"
+				+"Your scope is 0. You have 50 chips.", controller.get_result());
+	}
+	
 
 	
 	
