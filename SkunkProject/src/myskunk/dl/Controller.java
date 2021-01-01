855ef6a7e957075f60c8b4f7b6255a4c8a84cf23
yang1014-st <yang1014@stthomas.edu>
2020-12-31 23:11:42 -0600

    Add Unit Test for Controller - test_1turn_choose_n - Red

--- SkunkProject/src/myskunk/dl/Controller.java
+++ SkunkProject/src/myskunk/dl/Controller.java
@@ -5,18 +5,14 @@ import edu.princeton.cs.introcs.StdOut;
 
 public class Controller {
 
-	public static  void run (){
+	public static void run() {
 
 		Game controller = new Game();
 
 		char wants_to_roll;
-		
-		
-		StdOut.println("Welcome to play Skunk!");
-		StdOut.println("Do you want to roll? y or n =>");
-		wants_to_roll = StdIn.readLine().toLowerCase().charAt(0);
 
-	
+		StdOut.println("Welcome to play Skunk!");
+		wants_to_roll = ask_user_if_want_to_roll();
 
 		if (wants_to_roll == 'y') {
 
@@ -25,22 +21,39 @@ public class Controller {
 				StdOut.println(controller.get_message_after_each_roll());
 
 				if (controller.get_turn().getLastRoll().get_result_of_check_skunk() == "not skunk") {
-					StdOut.println("Do you want to roll? y or n =>");
-					wants_to_roll = StdIn.readLine().toLowerCase().charAt(0);
+					wants_to_roll = ask_user_if_want_to_roll();
 				}
 
 				else {
 					break;
 				}
 			}
+			StdOut.println("You did not select 'y'");
 			controller.end_game();
 			StdOut.println(controller.get_message_after_each_turn());
 
-		} else if (wants_to_roll == 'n') {
+		} else  {
+			StdOut.println("You did not select 'y'");
 			controller.end_game();
 			StdOut.println(controller.get_message_after_each_turn());
 		}
 
+	}
+
+	private static char ask_user_if_want_to_roll() {
+		char wants_to_roll;
+		try {		
+			
+			StdOut.println("Do you want to roll? y or n =>");
+			wants_to_roll = StdIn.readLine().toLowerCase().charAt(0);
+			return wants_to_roll;
+		}
+		catch (StringIndexOutOfBoundsException e) {
+	
+			wants_to_roll ='n';
+			return wants_to_roll;
+		}
+		
 	}
 
 }
---
+++ SkunkProject/test/myskunk/dl/TestController.java
@@ -0,0 +1,14 @@
+package myskunk.dl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestController {
+
+	@Test
+	void test_1turn_choose_n() {
+		fail("Not yet implemented");
+	}
+
+}
