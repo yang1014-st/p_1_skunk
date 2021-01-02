e1231a6e5adf0b8714484b1b0314ebaf35362faa
yang1014-st <yang1014@stthomas.edu>
2021-01-02 12:46:40 -0600

    test_ask_user_if_want_to_roll - Red

--- SkunkProject/test/myskunk/dl/TestController.java
+++ SkunkProject/test/myskunk/dl/TestController.java
@@ -16,6 +16,17 @@ class TestController {
 		assertNotNull(controller.get_game());
 	}
 	
+	
+	@Test
+	void test_ask_user_if_want_to_roll() {
+		
+		Controller controller = new Controller();
+		controller.before_run(true);
+		assertEquals(true,controller.ask_user_if_want_to_roll(true, true));
+		assertEquals(false,controller.ask_user_if_want_to_roll(true, true));
+
+	}
+	
 
 
 	
