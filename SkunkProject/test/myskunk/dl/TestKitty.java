3fff299818323a2d3dc3d706ebf29c80ea9e75c8
yang1014-st <yang1014@stthomas.edu>
2021-01-02 12:21:22 -0600

    test_kitty - Red

---
+++ SkunkProject/test/myskunk/dl/TestKitty.java
@@ -0,0 +1,14 @@
+package myskunk.dl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestKitty {
+
+	@Test
+	void test_kitty() {
+		fail("Not yet implemented");
+	}
+
+}
