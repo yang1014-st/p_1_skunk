783dc7ac94dc66f7dbc8f20564bc0690342e0bb2
yang1014-st <yang1014@stthomas.edu>
2021-01-02 11:55:58 -0600

    TestPlayer - test_initiate_player - Red

---
+++ SkunkProject/test/myskunk/dl/TestPlayer.java
@@ -0,0 +1,14 @@
+package myskunk.dl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPlayer {
+
+	@Test
+	void test_initiate_player() {
+		fail("Not yet implemented");
+	}
+
+}
