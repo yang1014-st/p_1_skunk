2c3683a52e0245dee533016c4b50603e3e7690e0
yang1014-st <yang1014@stthomas.edu>
2021-01-02 10:55:58 -0600

    test_with_predictable_die_1and1 - Red

---
+++ SkunkProject/test/myskunk/dl/TestTurn.java
@@ -0,0 +1,14 @@
+package myskunk.dl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestTurn {
+
+	@Test
+	void test_with_predictable_die_1and1() {
+		fail("Not yet implemented");
+	}
+
+}
